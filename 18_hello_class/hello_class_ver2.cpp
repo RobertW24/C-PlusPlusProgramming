2d3db64aeebdc26d986226438e2a1c43fb57bf5b
Robert Waterman <rwaterman0451@floridapoly.edu>
2026-06-18 19:01:39 +0000

    18

---
+++ 18_hello_class/hello_class_ver2.cpp
@@ -0,0 +1,77 @@
+
+#include <iostream>
+#include <string>
+
+class Hello {
+    
+    public:
+    // No arg constructor
+    Hello() : size(0), messages(nullptr) {
+        std::cout << "No arg constructor for " << this << std::endl; // this = pointer to current object
+    }
+    
+    // Constructor with args
+    Hello(int n) : size(n) {
+        std::cout << "Argument constructor for " << this << std::endl;
+        messages = new std::string[size];
+        // Initialize a dynamic array
+        for (int i = 0; i < size; i++) {
+            messages[i] = (i % 2) ? "Welcome!" : "Not welcome!";
+        }
+    }
+    
+    // Copy constructor
+    Hello(const Hello& other) {
+        std::cout << "Copy constructor for " << this << std::endl;
+        size = other.size;
+        // Allocate new memory
+        messages = new std::string[size];
+        // Copy values
+        for (int i = 0; i < size; i++ ) {
+            messages[i] = other.messages[i];
+        }
+
+    }
+    
+    // Destrutor
+    ~Hello() {
+        std::cout << "Destructor for " << this << std::endl;
+        if (messages) {
+            delete [] messages;
+        }
+    }
+
+
+    void bye() const {
+        std::cout << "Bye!!!\n";
+    }
+
+private:
+    std::string* messages;
+    int size;
+
+};
+
+int main(void) {
+    // Create Hello object dynamically
+    Hello* hi = new Hello;
+
+    // Call bye method
+    hi->bye();
+
+    // Crate an object with argument constructotr
+    Hello* hi2 = new Hello(10);
+
+    // Deallocate the memory
+    delete hi;
+    delete hi2;
+
+    // for (int i = 0; i < 100; i++) {
+    //     Hello(9999999);
+    // }
+
+    Hello hi3(20);
+    Hello hi4 = hi3;
+
+    return 0;
+}
