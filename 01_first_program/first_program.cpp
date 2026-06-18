0d06fc55c09316c3c141254536430c064f84c1fb
Robert Waterman <rwaterman0451@floridapoly.edu>
2026-06-18 18:02:14 +0000

    1

---
+++ 01_first_program/first_program.cpp
@@ -0,0 +1,26 @@
+
+/*
+*   First C++ Program
+*   01/14/2026
+*
+*/
+
+#include <iostream> // input output stream
+
+// Create main funtion
+int main(void) {
+    // Print to the console
+    std::cout << "Hello from COP3337!\n" << std::endl;
+
+    std::cout << "45 * 98 - 90 is " << 45 * 98 - 90 << std::endl;
+
+    // std - standrd namespace
+    // :: - scope resolution operaor
+    // cout - console output (object of iostream class)
+    // << - stream insertion operator
+    // endl - end of line
+
+
+
+    return 0; // 0 = success; non-zero = error
+}
