82eeda85564c9090e6ec22b43df9ff9f2a3635ce
Robert Waterman <rwaterman0451@floridapoly.edu>
2026-06-18 18:41:33 +0000

    5

---
+++ 05_random_numbers/random_numbers.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <random>
+
+int main(void) {
+    // Generate seed
+    std::random_device rd;
+
+    // Create random number engine and initialize it with a seed
+    std::mt19937 eng(rd());
+
+    // Define a distribution
+    std::uniform_int_distribution<int> dist(1, 100);
+
+    for (int i = 0; i < 20; i++) {
+        int x = dist(eng);
+        std::cout << x << std::endl;
+    }
+
+
+
+    return 0;
+}
